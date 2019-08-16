c97f31f67c8ba00f7d52fd8c7c0974585ab17ef7
Paul Meteng <meteng@publicplan.de>
2019-08-16 07:42:10 +0200

    carousel angepasst

--- src/components/dropdowns.js
+++ src/components/dropdowns.js
@@ -3,7 +3,7 @@ import Lectures from './lectures';
 import Filter from './filter';
 
 const Dropdowns = () => (
-  <div className="col-2 col-sm-3 inline mx-5">
+  <div className="col-auto col-md-3 col-sm-3 inline mx-5">
     <Lectures />
     <Filter />
   </div>
