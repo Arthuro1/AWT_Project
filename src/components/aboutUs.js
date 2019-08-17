0102fe9e966b5237e1fd4e50fe5426cff6a72e66
Paul Meteng <meteng@publicplan.de>
2019-08-17 14:30:49 +0200

    person's cars is now responsive

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import Header from './header';
 import Footer from './footer';
 import SectionTitle from './sectionTitle';
-
+import PersonsCard from './personCard';
 import paul from '../images/paul.jpeg';
 import shahrzad from '../images/shahrzad.jpg';
 import meijie from '../images/Meijie.jpg';
@@ -11,135 +11,93 @@ import vidhu from '../images/Vidhu.jpg';
 import groupselfii from '../images/groupselfii.jpg';
 
 export default class AboutUs extends Component {
-  render() {
+    render() {
 
-    return (
-      <div>
-        <Header/>
-        <section className="home-slider-loop-false inner-page owl-carousel">
-          <div className="slider-item myBgimage">
-            <div className="container">
-              <div className="row slider-text align-items-center justify-content-center">
-                <div className="col-md-8 text-center col-sm-12 element-animate">
-                  <h1>About Us</h1>
+        return (
+            <div>
+                <Header/>
+                <section className="home-slider-loop-false inner-page owl-carousel">
+                    <div className="slider-item myBgimage">
+                        <div className="container">
+                            <div className="row slider-text align-items-center justify-content-center">
+                                <div className="col-md-8 text-center col-sm-12 element-animate">
+                                    <h1>About Us</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <div className="section container-fluid myBg">
+                    <SectionTitle title="#-GROUP" backText=""/>
+                    <div className="row">
+                        <div className="col-lg-6 bg-transparent grouppic">
+                            <img height="90%" width="95%" src={groupselfii} alt="images"/>
+                        </div>
+
+                        <div className="col-lg-6 pl-2 pl-lg-5">
+                            <h2 className="mb-5"><b>History</b></h2>
+                            <p>Look at this beautiful photo!</p>
+                            <p>
+                                Yes, this is us! And we are the group <b><i>#-GROUP</i></b>. It’s really an
+                                interesting name, isn’t it? Before talking about this name, let's just begin with us.
+                                From the left to right, that’s
+                                <b><i> Shahrzad Amini</i></b>, <b><i> Paul Meteng</i></b>, <b><i> Meijie
+                                Li</i></b> and <b><i> Vidhya Vijayaraman</i></b>.
+                                Just years ago, we all lived in different countries, that’s
+                                Iran, Kamerun, China and Indien.
+                            </p>
+                            <p>
+                                A few years ago, we all lived in different countries which are respectively in Iran,
+                                Cameroon, China and India. But now we are in the same group and have finished this
+                                project together, that’s really amazing.
+                                How could that come true? That just because we took part in an amazing
+                                course <b><i> Advanced Web Technology </i></b> together! Yes, we are all the student
+                                from
+                                Duisburg-Essen university and we learn Computer Engineering. And this website was the
+                                result of our project in this course.
+                            </p>
+
+                            <p className="mb-5">
+                                The goal was to come up with an idea that would facilitate students's lives and help
+                                them during their studies. We created this platform where new students can easily find
+                                the right books for their studies.
+                                In fact students can easily find the most recommended books for a particular subject
+                                based on the rating and comments of others. They can also rate and comment the books for
+                                others.
+                            </p>
+                            <p>
+                                Finally, let's come back to the meaning of our interesting group name <b><i>#-GROUP</i></b>.
+                                But why actually <b><i> ‘#’</i></b>? That is just because during the preparation of the
+                                React
+                                hands-on session and lecture one of our group member, while copying some code from
+                                internet accidentally copied a #. It is only later we realised that the hash didn't have
+                                any meaning in the code. At that time we had already pasted it in multiple places in our
+                                code.
+                                It is from this hilarious situation that the name of our group came from, remembering us
+                                actually what copy-paste can cause.
+                            </p>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </section>
 
-        <div className="section container-fluid myBg">
-          <SectionTitle title="GROUP #" backText=""/>
-          <div className="row">
-            <div className="col-lg-6 bg-transparent grouppic">
-              <img height="90%" width="95%" src={groupselfii} alt="images"/>
-            </div>
+                <section className="section bg-light">
+                    <SectionTitle title="Group Members" backText="Members"/>
 
-            <div className="col-lg-6 pl-2 pl-lg-5">
-              <h2 className="mb-5">History</h2>
-              <p>Look this beautiful photo!.</p>
-              <p>
-                  Yes, this is us! And we are the group ‘AWT# ‘ It’s really an
-                  interesting name, isn’t it? Before talking about this name, now
-                  let's just begin with us. From the left to right, that’s
-                  Shahrzad Amini, Paul Meteng, Meijie Li, and Vidhya Vijayaraman.
-                  Just years ago, we all lived in different countries, that’s
-                  Iran, Kamerun, China and Indien.
-              </p>
-              <p>
-                  But now we are in the same group and have finished this project
-                  together, that’s really amazing. How can that come true? That
-                  just because we took part in an amazing course ‘Advanced Web
-                  Technology ’ together! Yes, we are all the student from
-                  Duisburg-Essen university and of course, we learned computer
-                  engineering. And this website is our project in this course.{' '}
-              </p>
+                    <div className="row px-auto mx-5">
+                        <div className=" box col-lg-6 col-md-12 col-auto ">
+                            <PersonsCard photo={paul} name="Paul Meteng" role="Frontend & Backend"/>
+                            <PersonsCard photo={shahrzad} name="Shahrzad Amini" role="Frontend"/>
+                        </div>
+                        <div className="box col-lg-6 col-md-12 col-auto ">
+                            <PersonsCard photo={meijie} name="Meijie Li" role="Backend"/>
+                            <PersonsCard photo={vidhu} name="Vidhya Vijayaraman" role="Frontend"/>
+                        </div>
+                    </div>
+                </section>
 
-              <p className="mb-5">
-                  We began with the idea that wants to help new students more
-                  clearly to choose their courses and we thought books maybe a
-                  very good idea to help students know this subject well.{' '}
-              </p>
-              <p>
-                  Most important is that you can share your inspirations here, so
-                  it also likes a communication platform for all the people who
-                  want to learn this subject.
-              </p>
-              <p>
-                {' '}
-                  So it's also very useful for the people who have learned this
-                  subject. And we also have many good ideas haven't made it come
-                  true, we may add it later.
-              </p>
-              <p>
-                  At last, let talk about our interesting group name ‘AWT#’, you
-                  may have already guessed the meaning of ‘AWT ’, just the
-                  abbreviation of the name of our course ‘Advanced Web
-                  Technology’. And the why ‘#’? That just because “During react
-                  hands-on and lecture practice session we added # in code so we
-                  called our team as" hash".
-              </p>
+                <Footer/>
             </div>
-          </div>
-        </div>
-
-        <section className="section bg-light">
-          <SectionTitle title="Group Members" backText="Members"/>
-
-          <div className="box">
-            <div className="card">
-              <div className="imgBx">
-                <img src={paul} alt="images"/>
-              </div>
-              <div className="details">
-                <h2>
-                    Paul Meteng
-                  <br/>
-                  <span>Frontend & Backend</span>
-                </h2>
-              </div>
-            </div>
-            <div className="card">
-              <div className="imgBx">
-                <img src={shahrzad} alt="images"/>
-              </div>
-              <div className="details">
-                <h2>
-                    Shahrzad Amini
-                  <br/>
-                  <span>Frontend</span>
-                </h2>
-              </div>
-            </div>
-            <div className="card">
-              <div className="imgBx">
-                <img src={meijie} alt="images"/>
-              </div>
-              <div className="details">
-                <h2>
-                    Meijie Li
-                  <br/>
-                  <span>Backend</span>
-                </h2>
-              </div>
-            </div>
-            <div className="card">
-              <div className="imgBx">
-                <img src={vidhu} alt="images"/>
-              </div>
-              <div className="details">
-                <h2>
-                    Vidhya Vijayaraman
-                  <br/>
-                  <span>Frontend</span>
-                </h2>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        <Footer/>
-      </div>
-    );
-  }
+        );
+    }
 }
